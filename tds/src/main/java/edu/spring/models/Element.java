d6e5287f829ce929d8a4fce2b6a753235799f146
Léo <22008676@etu.unicaen.fr>
2023-01-20 12:47:56 +0100

    Objet métier

---
+++ tds/src/main/java/edu/spring/models/Element.java
@@ -0,0 +1,21 @@
+package edu.spring.models;
+
+public class Element {
+
+    private String nom;
+    private String evaluation;
+    public String getNom() {
+        return nom;
+    }
+    public String getEvaluation() {
+        return evaluation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Element el){
+            return this.nom.equals(el.getNom());
+        }
+        return false;
+    }
+}
